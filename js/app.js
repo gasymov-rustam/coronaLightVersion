75f78fa574f26d93237b52574434aa71665e8a83
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-09-13 12:06:40 +0300

    received information of corona

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 const wrapperTabsEl = document.getElementById("wrapperTabs");
 const wrapperContainerTabsEl = document.getElementById("wrapperContainer");
+const currentTime = new Date(Date.now()).toISOString().slice(0, 10);
+let data = [];
+
+
+axios.get(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
+  .then(res => {
+    data = res.data;
+    console.log(data);
+  })
+  .catch(error => console.warn(error))
 
 wrapperTabsEl.addEventListener('click', e => {
   const region = e.target.dataset.region;
