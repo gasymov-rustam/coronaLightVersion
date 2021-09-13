58e5341f6c5ab68358d0d474c58356dd812a3302
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-09-13 11:58:01 +0300

    was initialized variables

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 const wrapperTabsEl = document.getElementById("wrapperTabs");
 const wrapperContainerTabsEl = document.getElementById("wrapperContainer");
+
+wrapperTabsEl.addEventListener('click', e => {
+  const region = e.target.dataset.region;
+  console.log(region);
+})
