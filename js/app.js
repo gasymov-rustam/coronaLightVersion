8e87cebec140ffea3363f1848df4ea1aef686acf
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-09-13 22:27:00 +0300

    tabs ready to work

--- js/app.js
+++ js/app.js
@@ -1,37 +1,105 @@
-const wrapperTabsEl = document.getElementById("wrapperTabs");
 const wrapperContainerTabsEl = document.getElementById("wrapperContainer");
+const wrapperInformationEl = document.getElementById("generalInformation");
+const wrapperTabsEl = document.getElementById("wrapperTabs");
 const currentTime = new Date(Date.now()).toISOString().slice(0, 10);
-let data = [];
+let region = 'ukraine';
+let coronaData = [];
 
 
 axios.get(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
-  .then(res => {
-    data = res.data;
-    console.log(data);
-  })
-  .catch(error => console.warn(error))
+    .then(res => {
+        coronaData = res.data;
+        renderCoronaData(wrapperContainerTabsEl, coronaData, [region])
+        renderToHtmlGeneralInformation(wrapperInformationEl, coronaData, [region])
+    })
+    .catch(error => console.warn(error))
 
 wrapperTabsEl.addEventListener('click', e => {
-  const region = e.target.dataset.region;
-  console.log(region);
+    region = e.target.dataset.region;
+    Array.from(wrapperTabsEl.children).forEach(el => {
+        el.classList.remove('active');
+        e.target.classList.add('active')
+    })
+    renderCoronaData(wrapperContainerTabsEl, coronaData, [region])
+    renderToHtmlGeneralInformation(wrapperInformationEl, coronaData, [region])
 })
 
-function renderCoronaDataFirst(elemForRender, dataArray) {
-  elemForRender.innerHTML = createDataArrFirst(dataArray).join('');
+function renderCoronaData(elemForRender, dataArray, smartKey) {
+    elemForRender.innerHTML = createDataArr(dataArray, smartKey).join('');
 }
 
-function createDataArrFirst(dataArray) {
-  return dataArray.map((field) => createDataFieldFirst(field));
+function createDataArr(dataArray, smartKey) {
+    return dataArray[smartKey].map((field) => createDataField(field));
 }
 
-function createDataFieldFirst(field) {
-  return `<dl class="wrapper-data">
-            <dt class="wrapper-data__country">${field?.label?.uk}</dt>
+function createDataField(array) {
+    return `<dl class="wrapper-data">
+            <dt class="wrapper-data__country">${array?.label?.uk}</dt>
             <dd class="wrapper-data__confirmed">
-              ${field?.confirmed}
+              <p>${array?.confirmed}</p>
+              ${createDataDifference(array, ['delta_confirmed'])}
             </dd>
-            <dd class="wrapper-data__deaths">${field?.deaths}</dd>
-            <dd class="wrapper-data__recovered">${field?.recovered}</dd>
-            <dd class="wrapper-data__existing">${field?.existing}</dd>
+            <dd class="wrapper-data__deaths">
+                <p>${array?.deaths}</p>
+                ${createDataDifference(array, ['delta_deaths'])}
+            </dd>
+            <dd class="wrapper-data__recovered">
+                <p>${array?.recovered}</p>
+                ${createDataDifference(array, ['delta_recovered'])}
+            </dd>
+            <dd class="wrapper-data__existing">
+                <p>${array?.existing}</p>
+                ${createDataDifference(array, ['delta_existing'])}
+            </dd>
           </dl>`;
+}
+
+function createDataDifference(array, smartKey) {
+    let difference = '';
+    if (array[smartKey] > 0) difference = `<p><i class="fas fa-arrow-up"></i> ${array[smartKey]}</p>`;
+    if (array[smartKey] < 0) difference = `<p><i class="fas fa-arrow-down"></i></i> ${array[smartKey]}</p>`;
+    if (array[smartKey] === 0) difference = `<p> - </p>`;
+    return difference;
+}
+
+function renderToHtmlGeneralInformation(element, array, region) {
+    element.innerHTML = createHtmlGeneralInformation(array, region);
+}
+
+function createHtmlGeneralInformation(array, region) {
+    return `<div class="general-information__confirmed">
+                <p>Виявлено:</p>
+                <p>${createGeneralInformation(array, region, 'confirmed')}</p>
+                ${createGeneralDifference(array, region, 'delta_confirmed')}
+            </div>
+            <div class="general-information__deaths">
+                <p>Померло:</p>
+                <p>${createGeneralInformation(array, region, 'deaths')}</p>
+                ${createGeneralDifference(array, region, 'delta_deaths')}
+            </div>
+            <div class="general-information__recovered">
+                <p>Одужали:</p>
+                <p>${createGeneralInformation(array, region, 'recovered')}</p>
+                ${createGeneralDifference(array, region, 'delta_recovered')}
+            </div>
+            <div class="general-information__existing">
+                <p>Выздровили:</p>
+                <p>${createGeneralInformation(array, region, 'existing')}</p>
+                ${createGeneralDifference(array, region, 'delta_existing')}
+            </div>`
+}
+
+function createGeneralDifference(array, region, smartKey) {
+    let difference = '';
+    if (createGeneralInformation(array, region, smartKey) > 0) difference = `<p class="general-information__differnce"><i class="fas fa-arrow-up"></i> ${createGeneralInformation(array, region, smartKey)}</p>`;
+    if (createGeneralInformation(array, region, smartKey) < 0) difference = `<p class="general-information__differnce"><i class="fas fa-arrow-down"></i></i> ${createGeneralInformation(array, region, smartKey)}</p>`;
+    if (createGeneralInformation(array, region, smartKey) === 0) difference = `<p class="general-information__differnce"> - </p>`;
+    return difference;
+}
+
+function createGeneralInformation(array, region, smartKey) {
+    return array[region].reduce((total, item) => {
+        total += item[smartKey]
+        return total;
+    }, 0)
 }
