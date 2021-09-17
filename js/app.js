3e934a1209650983507031c821a3ad16c1caa4a2
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-09-17 13:42:25 +0300

    last release

--- js/app.js
+++ js/app.js
@@ -3,43 +3,42 @@ const wrapperInformationEl = document.getElementById("generalInformation");
 const wrapperTabsEl = document.getElementById("wrapperTabs");
 const searchFormEl = document.getElementById("searchFormEl");
 const wrapperBtn = document.getElementById("wrapperSort");
-const currentTime = new Date(Date.now()).toISOString().slice(0, 10);
-let region = 'ukraine';
-let coronaData = [];
+const currentTime = new Date().toJSON().slice(0, 10);
+let region = "ukraine";
+let coronaData = {};
 let newCoronaData = [];
 
-let counter = 0;
 let order = 1;
 
-axios.get(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
-    .then(res => {
+axios
+    .get(`https://api-covid19.rnbo.gov.ua/data?to=${currentTime}`)
+    .then((res) => {
         coronaData = res.data;
-        renderCoronaData(wrapperContainerTabsEl, coronaData, [region])
-        renderToHtmlGeneralInformation(wrapperInformationEl, coronaData, [region])
+        renderCoronaData(wrapperContainerTabsEl, coronaData[region]);
+        renderToHtmlGeneralInformation(wrapperInformationEl, coronaData[region]);
     })
-    .catch(error => console.warn(error))
+    .catch((error) => console.warn(error));
 
-wrapperTabsEl.addEventListener('click', e => {
-    region = e.target.dataset.region;
-    Array.from(wrapperTabsEl.children).forEach(el => {
-        el.classList.remove('active');
-        e.target.classList.add('active')
-    })
-    searchFormEl.reset();
-    renderCoronaData(wrapperContainerTabsEl, coronaData, [region])
-    renderToHtmlGeneralInformation(wrapperInformationEl, coronaData, [region])
-    let newarr = coronaData[region];
-})
+wrapperTabsEl.addEventListener("click", (e) => {
+    const btn = e.target.closest("button");
+    if (btn) {
+        region = btn.dataset.region;
+        Array.from(wrapperTabsEl.children).forEach((el) => {
+            el.classList.remove("active");
+            btn.classList.add("active");
+        });
+        searchFormEl.reset();
+        renderCoronaData(wrapperContainerTabsEl, coronaData[region]);
+        renderToHtmlGeneralInformation(wrapperInformationEl, coronaData[region])
+    }
+});
 
-
-
-function renderCoronaData(elemForRender, dataArray, region) {
-    elemForRender.innerHTML = createDataArr(dataArray, region).join('');
+function renderCoronaData(elemForRender, array) {
+    elemForRender.innerHTML = createDataArr(array).join("");
 }
 
-function createDataArr(dataArray, region) {
-    if (Array.isArray(dataArray)) return dataArray.map((field) => createDataField(field));
-    else return dataArray[region].map((field) => createDataField(field));
+function createDataArr(array) {
+    return array.map((field) => createDataField(field));
 }
 
 function createDataField(array) {
@@ -47,110 +46,100 @@ function createDataField(array) {
             <dt class="wrapper-data__country">${array?.label?.uk}</dt>
             <dd class="wrapper-data__confirmed">
               <p>${array?.confirmed}</p>
-              ${createDataDifference(array, ['delta_confirmed'])}
+              <p  class="wrapper-data__delta">${createDataDifference(array["delta_confirmed"])}</p>
             </dd>
             <dd class="wrapper-data__deaths">
                 <p>${array?.deaths}</p>
-                ${createDataDifference(array, ['delta_deaths'])}
+                <p  class="wrapper-data__delta">${createDataDifference(array["delta_deaths"])}</p>
             </dd>
             <dd class="wrapper-data__recovered">
                 <p>${array?.recovered}</p>
-                ${createDataDifference(array, ['delta_recovered'])}
+                <p  class="wrapper-data__delta">${createDataDifference(array["delta_recovered"])}</p>
             </dd>
             <dd class="wrapper-data__existing">
                 <p>${array?.existing}</p>
-                ${createDataDifference(array, ['delta_existing'])}
+                <p  class="wrapper-data__delta">${createDataDifference(array["delta_existing"])}</p>
             </dd>
           </dl>`;
 }
 
-function createDataDifference(array, smartKey) {
-    let difference = '';
-    if (array[smartKey] > 0) difference = `<p  class="wrapper-data__delta"><i class="fas fa-arrow-up"></i> ${array[smartKey]}</p>`;
-    if (array[smartKey] < 0) difference = `<p  class="wrapper-data__delta"><i class="fas fa-arrow-down"></i></i> ${array[smartKey]}</p>`;
-    if (array[smartKey] === 0) difference = `<p  class="wrapper-data__delta"> - </p>`;
-    return difference;
+function createDataDifference(count) {
+    return `${count ? `<i class="fas fa-arrow-${count > 0 ? "up" : "down"}"></i> ${count}` : "-"}`;
 }
 
-function renderToHtmlGeneralInformation(element, array, region) {
-    element.innerHTML = createHtmlGeneralInformation(array, region);
+function renderToHtmlGeneralInformation(element, array) {
+    element.innerHTML = createHtmlGeneralInformation(array);
 }
 
-function createHtmlGeneralInformation(array, region) {
+function createHtmlGeneralInformation(array) {
     return `<div class="general-information__confirmed">
                 <p>Виявлено:</p>
-                <p>${createGeneralInformation(array, region, 'confirmed')}</p>
-                ${createGeneralDifference(array, region, 'delta_confirmed')}
+                <p>${createGeneralInformation(array, "confirmed")}</p>
+                <p  class="wrapper-data__delta">${createDataDifference(createGeneralInformation(array, "delta_confirmed"))}</p>     
             </div>
             <div class="general-information__deaths">
                 <p>Померло:</p>
-                <p>${createGeneralInformation(array, region, 'deaths')}</p>
-                ${createGeneralDifference(array, region, 'delta_deaths')}
+                <p>${createGeneralInformation(array, "deaths")}</p>
+                <p  class="wrapper-data__delta">${createDataDifference(createGeneralInformation(array, "delta_deaths"))}</p>     
             </div>
             <div class="general-information__recovered">
                 <p>Одужали:</p>
-                <p>${createGeneralInformation(array, region, 'recovered')}</p>
-                ${createGeneralDifference(array, region, 'delta_recovered')}
+                <p>${createGeneralInformation(array, "recovered")}</p>
+                <p  class="wrapper-data__delta">${createDataDifference(createGeneralInformation(array, "delta_recovered"))}</p>     
             </div>
             <div class="general-information__existing">
                 <p>Выздровили:</p>
-                <p>${createGeneralInformation(array, region, 'existing')}</p>
-                ${createGeneralDifference(array, region, 'delta_existing')}
-            </div>`
+                <p>${createGeneralInformation(array, "existing")}</p>
+                <p  class="wrapper-data__delta">${createDataDifference(createGeneralInformation(array, "delta_existing"))}</p>     
+            </div>`;
 }
 
-function createGeneralDifference(array, region, smartKey) {
-    let difference = '';
-    if (createGeneralInformation(array, region, smartKey) > 0) difference = `<p class="general-information__differnce"><i class="fas fa-arrow-up"></i> ${createGeneralInformation(array, region, smartKey)}</p>`;
-    if (createGeneralInformation(array, region, smartKey) < 0) difference = `<p class="general-information__differnce"><i class="fas fa-arrow-down"></i></i> ${createGeneralInformation(array, region, smartKey)}</p>`;
-    if (createGeneralInformation(array, region, smartKey) === 0) difference = `<p class="general-information__differnce"> - </p>`;
-    return difference;
-}
-
-function createGeneralInformation(array, region, smartKey) {
-    return array[region].reduce((total, item) => {
-        total += item[smartKey]
+function createGeneralInformation(array, key) {
+    return array.reduce((total, item) => {
+        total += item[key];
         return total;
-    }, 0)
+    }, 0);
 }
 
-searchFormEl.addEventListener('keyup', e => {
+searchFormEl.addEventListener("keyup", (e) => {
     e.preventDefault();
-    const searchField = ['uk', 'en'];
-    const query = String(e.target.value.trim().toLowerCase().split(' ').filter(word => !!word).slice(0, 1));
-    newCoronaData = coronaData[region].filter(country => {
-        return searchField.some(field => {
-            return String(country.label[field]).toLowerCase().includes(query)
-        })
-    })
-    renderCoronaData(wrapperContainerTabsEl, newCoronaData)
-})
+    const searchField = ["uk", "en"];
+    const query = String(
+        e.target.value
+            .trim()
+            .toLowerCase()
+            .split(" ")
+            .filter((word) => !!word)
+            .slice(0, 1)
+    );
+    newCoronaData = coronaData[region].filter((country) => {
+        return searchField.some((field) => {
+            return String(country.label[field]).toLowerCase().includes(query);
+        });
+    });
+    renderCoronaData(wrapperContainerTabsEl, newCoronaData);
+});
 
-function sortTo(arr, smartKey, order) {
-    if (typeof arr[0][smartKey]['uk'] === "string") {
-        return (a, b) => a[smartKey]['uk'].localeCompare(b[smartKey]['uk']) * order;
+function sortTo(arr, key, order) {
+    if (typeof arr[0][key]["uk"] === "string") {
+        return (a, b) => a[key]["uk"].localeCompare(b[key]["uk"]) * order;
     } else {
-        return (a, b) => (a[smartKey] - b[smartKey]) * order;
+        return (a, b) => (a[key] - b[key]) * order;
     }
 }
 
-wrapperBtn.addEventListener('click', e => {
-    const key = e.target.value;
-    const inscriptionSort = e.target.dataset.select;
-    let newCorona = coronaData[region];
-    Array.from(wrapperBtn.children).forEach(btn => {
-        const btnName= btn.dataset.select
-        btn.innerHTML = `${btnName}`})
-
-    if (order > 0) {
-        newCorona.sort(sortTo(newCorona, [key], order))
-        renderCoronaData(wrapperContainerTabsEl, newCorona, [region]);
-        order = -1;
-        e.target.innerHTML = `<i class="fas fa-arrow-down"></i> ${inscriptionSort}`;
-    } else {
-        newCorona.sort(sortTo(newCorona, [key], order))
-        renderCoronaData(wrapperContainerTabsEl, newCorona, [region]);
-        order = 1;
-        e.target.innerHTML = `<i class="fas fa-arrow-up"></i> ${inscriptionSort}`;
+wrapperBtn.addEventListener("click", (e) => {
+    const currentBtn = e.target.closest('button');
+    if(currentBtn){
+        const key = e.target.value;
+        const inscriptionSort = e.target.dataset.select;
+        Array.from(wrapperBtn.children).forEach((btn) => {
+            const btnName = btn.dataset.select;
+            btn.innerHTML = `${btnName}`;
+        });
+        coronaData[region].sort(sortTo(coronaData[region], [key], order));
+        renderCoronaData(wrapperContainerTabsEl, coronaData[region]);
+        order *= -1;
+        e.target.innerHTML = `<i class="fas fa-arrow-${order > 0 ? "down" : "up"}"></i> ${inscriptionSort}`;
     }
-})
+});
